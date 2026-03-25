ba0dc28f3043dcc66501c670d6859096752aca88
EmBerrrrrr <lan18051973@gmail.com>
2026-03-25 22:31:11 +0700

    d

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 import subprocess
 
 app = Flask(__name__)
-
 
 @app.route('/checkin')
 def checkin():
@@ -49,7 +48,55 @@ def checkout():
         print("CHECK-OUT ERROR:", e)
         return "DENY"
 
+@app.route('/face_checkin', methods=['POST'])
+def face_checkin():
+    try:
+        token = request.json.get("token", "")
+
+        result = subprocess.run(
+            ["python", "face_checkin_single_cam.py", "checkin", token],
+            capture_output=True,
+            text=True
+        )
+
+        print("\n===== FACE CHECKIN LOG =====")
+        print(result.stdout)
+        print(result.stderr)
+
+        if result.returncode == 0:
+            return "OPEN"
+        else:
+            return "DENY"
+
+    except Exception as e:
+        print("FACE CHECKIN ERROR:", e)
+        return "DENY"
+
+
+@app.route('/face_checkout', methods=['POST'])
+def face_checkout():
+    try:
+        token = request.json.get("token", "")
+
+        result = subprocess.run(
+            ["python", "face_checkin_single_cam.py", "checkout", token],
+            capture_output=True,
+            text=True
+        )
+
+        print("\n===== FACE CHECKOUT LOG =====")
+        print(result.stdout)
+        print(result.stderr)
+
+        if result.returncode == 0:
+            return "OPEN"
+        else:
+            return "DENY"
+
+    except Exception as e:
+        print("FACE CHECKOUT ERROR:", e)
+        return "DENY"
 
 if __name__ == "__main__":
-    print(" Flask API started")
+    print("Flask API started")
     app.run(host='0.0.0.0', port=5000)
